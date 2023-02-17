e4d09a9805ca4cef27b34d3c77a2cdda0bd5382d
john <jgrayson@esri.com>
2023-02-17 07:30:52 -0800

    Initial commit

---
+++ js/Application.js
@@ -0,0 +1,245 @@
+/*
+ Copyright 2022 Esri
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import AppBase from "./support/AppBase.js";
+import AppLoader from "./loaders/AppLoader.js";
+import SignIn from './apl/SignIn.js';
+import FeaturesList from './apl/FeaturesList.js';
+import ViewLoading from './apl/ViewLoading.js';
+import FeatureLayerStats from './FeatureLayerStats.js';
+
+class Application extends AppBase {
+
+  // PORTAL //
+  portal;
+
+  constructor() {
+    super();
+
+    // LOAD APPLICATION BASE //
+    super.load().then(() => {
+
+      // APPLICATION LOADER //
+      const applicationLoader = new AppLoader({app: this});
+      applicationLoader.load().then(({portal, group, map, view}) => {
+        //console.info(portal, group, map, view);
+
+        // PORTAL //
+        this.portal = portal;
+
+        // VIEW SHAREABLE URL PARAMETERS //
+        this.initializeViewShareable({view});
+
+        // USER SIGN-IN //
+        this.configUserSignIn();
+
+        // SET APPLICATION DETAILS //
+        this.setApplicationDetails({map, group});
+
+        // APPLICATION //
+        this.applicationReady({portal, group, map, view}).catch(this.displayError).then(() => {
+          // HIDE APP LOADER //
+          document.getElementById('app-loader').toggleAttribute('hidden', true);
+        });
+
+      }).catch(this.displayError);
+    }).catch(this.displayError);
+
+  }
+
+  /**
+   *
+   */
+  configUserSignIn() {
+
+    const signInContainer = document.getElementById('sign-in-container');
+    if (signInContainer) {
+      const signIn = new SignIn({container: signInContainer, portal: this.portal});
+    }
+
+  }
+
+  /**
+   *
+   * @param view
+   */
+  configView(view) {
+    return new Promise((resolve, reject) => {
+      if (view) {
+        require([
+          'esri/core/reactiveUtils',
+          'esri/widgets/Popup',
+          'esri/widgets/Home',
+          'esri/widgets/Search',
+          'esri/widgets/LayerList',
+          'esri/widgets/Legend'
+        ], (reactiveUtils, Popup, Home, Search, LayerList, Legend) => {
+
+          // VIEW AND POPUP //
+          view.set({
+            constraints: {snapToZoom: false},
+            highlightOptions: {
+              fillOpacity: 0.01,
+              haloColor: "crimson"
+            },
+            popup: new Popup({
+              dockEnabled: true,
+              dockOptions: {
+                buttonEnabled: false,
+                breakpoint: false,
+                position: "top-right"
+              }
+            })
+          });
+
+          // HOME //
+          const home = new Home({view});
+          view.ui.add(home, {position: 'top-left', index: 0});
+
+          // LEGEND //
+          const legend = new Legend({view: view});
+          view.ui.add(legend, {position: 'bottom-left', index: 0});
+
+          // SEARCH //
+          const search = new Search({view: view});
+          view.ui.add(search, {position: 'top-right', index: 0});
+
+          // LAYER LIST //
+          const layerList = new LayerList({
+            container: 'layer-list-container',
+            view: view,
+            visibleElements: {statusIndicators: true}
+          });
+
+          // VIEW LOADING INDICATOR //
+          const viewLoading = new ViewLoading({view: view});
+          view.ui.add(viewLoading, 'bottom-right');
+
+          resolve();
+        });
+      } else { resolve(); }
+    });
+  }
+
+  /**
+   *
+   * @param portal
+   * @param group
+   * @param map
+   * @param view
+   * @returns {Promise}
+   */
+  applicationReady({portal, group, map, view}) {
+    return new Promise(async (resolve, reject) => {
+      // VIEW READY //
+      this.configView(view).then(() => {
+
+        this.initializeWaterGapTest({view});
+
+        resolve();
+      }).catch(reject);
+    });
+  }
+
+  /**
+   *
+   * @param view
+   */
+  initializeWaterGapTest({view}) {
+    require([
+      'esri/core/reactiveUtils',
+      'esri/core/promiseUtils'
+    ], (reactiveUtils, promiseUtils) => {
+
+      const yearSlider = document.getElementById('year-slider');
+      const variablesList = document.getElementById('variables-list');
+      const statTypeOption = document.getElementById('stat-type-option');
+
+      const defaultRenderingOptions = {
+        variable: variablesList.value, //'total',
+        statType: statTypeOption.value // 'gap'
+      };
+
+      const hydroBasinsLevel7Layer = view.map.layers.find(layer => layer.title === 'Hydrobasins Level 7');
+      const hydroBasinsLevel7Stats = new FeatureLayerStats({
+        container: 'huc-test-panel', view,
+        featureLayer: hydroBasinsLevel7Layer,
+        ...defaultRenderingOptions
+      });
+
+      const hydroBasinsLevel5Layer = view.map.layers.find(layer => layer.title === 'Hydrobasins Level 5');
+      const hydroBasinsLevel5Stats = new FeatureLayerStats({
+        container: 'huc-test-panel', view,
+        featureLayer: hydroBasinsLevel5Layer,
+        ...defaultRenderingOptions
+      });
+
+      const waterProvincesLayer = view.map.layers.find(layer => layer.title === 'Water Provinces');
+      const waterProvincesStats = new FeatureLayerStats({
+        container: 'huc-test-panel', view,
+        featureLayer: waterProvincesLayer,
+        ...defaultRenderingOptions
+      });
+
+      const allLayerStats = [hydroBasinsLevel7Stats, hydroBasinsLevel5Stats, waterProvincesStats];
+
+      variablesList.addEventListener('calciteComboboxChange', () => {
+        allLayerStats.forEach(layerStats => {
+          layerStats.variable = variablesList.value;
+        });
+      });
+
+      statTypeOption.addEventListener('calciteSegmentedControlChange', () => {
+        allLayerStats.forEach(layerStats => {
+          layerStats.statType = statTypeOption.value;
+        });
+      });
+      yearSlider.addEventListener('calciteSliderInput', () => {
+        allLayerStats.forEach(layerStats => {
+          layerStats.year = yearSlider.value;
+        });
+      });
+
+      // HANDLE ABORT ERRORS //
+      const _handleAbortErrors = error => { !promiseUtils.isAbortError(error) && console.error(error); };
+
+      // ABORT CONTROLLER //
+      let abortController;
+      //
+      // DISPLAY POPUP FOR FEATURE(S) AT VIEW CENTER //
+      //
+      reactiveUtils.when(() => view.stationary, () => {
+        reactiveUtils.whenOnce(() => !view.updating).then(() => {
+          abortController?.abort();
+          abortController = new AbortController();
+          view.popup.fetchFeatures(view.toScreen(view.center), {signal: abortController.signal}).then((response) => {
+            response.allGraphicsPromise.then((graphics) => {
+              if (graphics?.length) {
+                view.popup.open({features: graphics});
+              } else {
+                view.popup.close();
+              }
+            });
+          }).catch(_handleAbortErrors);
+        }, {initial: true});
+      }, {initial: true});
+
+    });
+  }
+
+}
+
+export default new Application();
---
+++ js/FeatureLayerStats.js
@@ -0,0 +1,354 @@
+/*
+ Copyright 2022 Esri
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+/**
+ *
+ * FeatureLayerStats
+ *  - Element: apl-feature-layer-stats
+ *  - Description: Feature Layer Statistics
+ *
+ * Author:   John Grayson - Applications Prototype Lab - Esri
+ * Created:  2/3/2023 - 0.0.1 -
+ * Modified:
+ *
+ */
+
+class FeatureLayerStats extends HTMLElement {
+
+  static version = '0.0.1';
+
+  /**
+   * @type {HTMLElement}
+   */
+  container;
+
+  /**
+   * @type {boolean}
+   */
+  #enabled;
+
+  /**
+   * @type {MapView|SceneView}
+   */
+  #view;
+
+  /**
+   * @type {FeatureLayer}
+   */
+  #featureLayer;
+
+  #statisticsFieldNames = [
+    'domestic_demand',
+    'industry_demand',
+    'irrigation_demand',
+    'livestock_demand',
+    'total_demand',
+    'domestic_gap',
+    'industry_gap',
+    'irrigation_gap',
+    'livestock_gap',
+    'total_gap'
+  ];
+
+  #onStatisticField;
+
+  #statType;
+  get statType() {
+    return this.#statType;
+  }
+
+  set statType(value) {
+    this.#statType = value;
+    this.updateStatsField();
+    this.updateStatsRenderer();
+  }
+
+  #variable;
+  get variable() {
+    return this.#variable;
+  }
+
+  set variable(value) {
+    this.#variable = value;
+    this.updateStatsField();
+    this.updateStatsRenderer();
+  }
+
+  #variableInfos = [
+    ['domestic_demand', {min: 0.01, max: 1.0}],
+    ['industry_demand', {min: 0.05, max: 1.0}],
+    ['irrigation_demand', {min: 0.05, max: 1.0}],
+    ['livestock_demand', {min: 0.001, max: 1.0}],
+    ['total_demand', {min: 0.05, max: 1.0}],
+    ['domestic_gap', {min: 0.0001, max: 1.0}],
+    ['industry_gap', {min: 0.0001, max: 1.0}],
+    ['irrigation_gap', {min: 0.0001, max: 1.0}],
+    ['livestock_gap', {min: 0.0001, max: 1.0}],
+    ['total_gap', {min: 0.05, max: 1.0}]
+  ];
+
+  #yearMin;
+  #yearOffset;
+  #year;
+  set year(value) {
+    this.#year = value;
+    this.#yearOffset = (this.#year - this.#yearMin);
+    this.fieldNameLabel.innerHTML = this.valueExpressionTitle;
+    this.updateStatsRenderer();
+  }
+
+  get valueExpression() {
+    return `Number(Split($feature.${ this.#onStatisticField },'|')[${ this.#yearOffset }])`;
+  }
+
+  get valueExpressionTitle() {
+    //const fieldLabel = this.#featureLayer.getField(this.#onStatisticField).alias;
+    return `${ this.#variable } water ${ this.#statType } in ${ this.#yearMin + this.#yearOffset }`;
+  }
+
+  /**
+   *
+   */
+  constructor({container, view, featureLayer, statType, variable}) {
+    super();
+
+    // CONTAINER //
+    this.container = (container instanceof HTMLElement) ? container : document.getElementById(container);
+
+    // VIEW //
+    this.#view = view;
+    // FEATURE LAYER //
+    this.#featureLayer = featureLayer;
+
+    this.#statType = statType;
+    this.#variable = variable;
+
+    this.#yearMin = this.#year = 1980;
+    this.#yearOffset = 0;
+
+    this.#enabled = true;
+
+    // COUNT FORMATTER //
+    this.countFormatter = new Intl.NumberFormat('default', {minimumFractionDigits: 0, maximumFractionDigits: 0});
+    this.valueFormatter = new Intl.NumberFormat('default', {minimumFractionDigits: 2, maximumFractionDigits: 2});
+
+    const shadowRoot = this.attachShadow({mode: 'open'});
+    shadowRoot.innerHTML = `
+      <style>
+        :host {}
+        :host .sum-label{
+          font-size: 13pt;
+          font-weight: 600;
+        }    
+        :host calcite-icon[icon="check-circle"]{
+          --calcite-ui-icon-color: darkgreen;        
+        }
+        :host calcite-icon[icon="exclamation-mark-triangle"]{
+          --calcite-ui-icon-color: crimson;        
+        }
+      </style>
+      <calcite-block heading="${ this.#featureLayer.title || "Feature Layer Statistics" }" description="all features within the current view" collapsible open>
+        <calcite-block-section class="status-section" text="Status" status="idle" hidden>
+          <calcite-label layout="inline">          
+            <calcite-icon class="visible-icon" icon="check-circle" scale="s"></calcite-icon>          
+            <span>Visible</span>          
+            <calcite-icon class="suspended-icon" icon="check-circle" scale="s"></calcite-icon>
+            <span>Suspended</span>                    
+          </calcite-label>
+        </calcite-block-section>
+        <br>
+        <calcite-label layout="inline-space-between">
+          <span>sum of <span class="field-name-label"></span></span><span class="sum-label">---</span>
+        </calcite-label>                        
+      </calcite-block>   
+    `;
+
+    this.container?.append(this);
+  }
+
+  /**
+   *
+   */
+  connectedCallback() {
+
+    this.visibleIcon = this.shadowRoot.querySelector('.visible-icon');
+    this.suspendedIcon = this.shadowRoot.querySelector('.suspended-icon');
+    this.statusSection = this.shadowRoot.querySelector('.status-section');
+    this.sumLabel = this.shadowRoot.querySelector('.sum-label');
+    this.fieldNameLabel = this.shadowRoot.querySelector('.field-name-label');
+
+    this.#featureLayer.load().then(() => {
+      this.#featureLayer.set({outFields: this.#statisticsFieldNames});
+      this._initialize();
+    });
+
+  }
+
+  clearLabels() {
+    this.sumLabel.innerHTML = '---';
+  }
+
+  /**
+   *
+   * @private
+   */
+  _initialize() {
+    require([
+      'esri/core/reactiveUtils',
+      'esri/core/promiseUtils',
+      'esri/smartMapping/statistics/summaryStatistics',
+      'esri/smartMapping/renderers/color'
+    ], (reactiveUtils, promiseUtils, summaryStatistics, colorRendererCreator) => {
+
+      const _handleAbortErrors = error => { !promiseUtils.isAbortError(error) && console.error(error); };
+
+      // LAYER VISIBLE //
+      reactiveUtils.watch(() => this.#featureLayer.visible, visible => {
+        this.visibleIcon.setAttribute('icon', visible ? 'check-circle' : 'exclamation-mark-triangle');
+      }, {initial: true});
+
+      // LAYERVIEW //
+      this.#view.whenLayerView(this.#featureLayer).then(layerView => {
+
+        // SUSPENDED //
+        reactiveUtils.watch(() => layerView.suspended, suspended => {
+          this.statusSection.setAttribute('status', suspended ? 'invalid' : 'valid');
+          this.suspendedIcon.setAttribute('icon', suspended ? 'exclamation-mark-triangle' : 'check-circle');
+          suspended && this.clearLabels();
+        }, {initial: true});
+
+        // NOT SUSPENDED | NOT UPDATING //
+        let abortController;
+        this.updateStatsRenderer = () => {
+          if (!layerView.suspended) {
+            reactiveUtils.whenOnce(() => !layerView.updating).then(() => {
+              abortController?.abort();
+              abortController = new AbortController();
+              this._updateStatsRenderer({signal: abortController.signal}).catch(_handleAbortErrors);
+            }, {initial: true});
+          } else {
+            this.clearLabels();
+          }
+        };
+
+        /**
+         *
+         */
+        this._updateStatsRenderer = ({signal}) => {
+          return new Promise((resolve, reject) => {
+            summaryStatistics({
+              view: this.#view,
+              layer: this.#featureLayer,
+              valueExpression: this.valueExpression,
+              useFeaturesInView: true,
+              signal
+            }).then((stats) => {
+              if (!signal.aborted) {
+                //console.info(this.#featureLayer.title, stats);
+
+                this.sumLabel.innerHTML = this.valueFormatter.format(stats.sum);
+
+                const colorVV = this.#featureLayer.renderer.visualVariables.find(vv => vv.type === 'color');
+                colorVV.set({
+                  valueExpressionTitle: this.valueExpressionTitle,
+                  valueExpression: this.valueExpression,
+                  legendOptions: {
+                    title: this.valueExpressionTitle
+                  }/*,
+                   stops: colorVV.stops.map((colorStop, colorStopIdx) => {
+                   const stopValue = (stats.min + ((stats.max - stats.min) * (colorStopIdx / colorVV.stops.length)));
+                   colorStop.value = stopValue;
+                   (colorStopIdx % 2 === 0) && (colorStop.label = stopValue.toFixed(2));
+                   return colorStop;
+                   })*/
+                });
+
+                /*const stopValues = {
+                 avgMinus: stats.avg - (stats.stddev * 0.5),
+                 average: stats.avg,
+                 avgPlus: stats.avg + (stats.stddev * 0.5)
+                 };
+
+                 this.#featureLayer.renderer = {
+                 type: 'simple',
+                 symbol: {
+                 type: 'simple-fill',
+                 color: 'rgba(255,255, 255,0.5)',
+                 outline: {
+                 width: 0.2,
+                 color: 'rgba(255,255, 255,1.0)'
+                 }
+                 },
+                 defaultSymbol: {
+                 type: 'simple-fill',
+                 color: 'rgba(127,127, 127,0.5)',
+                 outline: {
+                 width: 0.5,
+                 color: 'rgba(127,127, 127,0.9)'
+                 }
+                 },
+                 visualVariables: [
+                 {
+                 type: 'color',
+                 valueExpressionTitle: this.valueExpressionTitle,
+                 valueExpression: this.valueExpression,
+                 stops: [
+                 {
+                 label: `+half stdev - ${ stopValues.avgPlus.toFixed(6) }`,
+                 value: stopValues.avgPlus,
+                 color: [0, 110, 255]
+                 },
+                 {
+                 label: `average - ${ stopValues.average.toFixed(6) }`,
+                 value: stopValues.average,
+                 color: [191, 233, 255]
+                 },
+                 {
+                 label: `-half stdev - ${ stopValues.avgMinus.toFixed(6) }`,
+                 value: stopValues.avgMinus,
+                 color: [0, 110, 255]
+                 }
+                 ]
+                 }
+                 ]
+                 };*/
+
+                resolve();
+              }
+            }).catch(reject);
+          });
+        };
+
+        // UPDATE STATISTICS FIELD //
+        this.updateStatsField = () => {
+          this.#onStatisticField = `${ this.#variable }_${ this.#statType }`;
+          this.fieldNameLabel.innerHTML = this.valueExpressionTitle;
+        };
+        this.updateStatsField();
+
+        // WHEN STATIONARY //
+        reactiveUtils.when(() => this.#view.stationary, () => {
+          this.updateStatsRenderer();
+        }, {initial: true});
+
+      });
+    });
+  }
+
+}
+
+customElements.define("apl-feature-layer-stats", FeatureLayerStats);
+
+export default FeatureLayerStats;
