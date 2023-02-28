37e7430afb21b75f12e44c8718720398dd8bf40e
john <jgrayson@esri.com>
2023-02-27 17:46:04 -0800

    - small tweaks

--- js/FeatureLayerStats.js
+++ js/FeatureLayerStats.js
@@ -81,7 +81,7 @@ class FeatureLayerStats extends HTMLElement {
     'livestock_demand': {min: 0.001, max: 9.2, precision: 3},
     'livestock_gap': {min: 0.0001, max: 1.4, precision: 4},
     'total_demand': {min: 0.05, max: 1920.0, precision: 2},
-    'total_gap': {min: 0.05, max: 250.0, precision: 3}
+    'total_gap': {min: 0.05, max: 250.0, precision: 2}
   };
 
   #colorRampByVariable = {
